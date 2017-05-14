26c38dd6560e2186fb3fd141976830d95f892494
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-05-14 15:30:49 +0100

    Added html

--- render.php
+++ render.php
@@ -1,1 +1,16 @@
+<html>
+  <head>
+    <style>
+      table.irc tr td {
+        height: 40px;
+        width: 40px;
+        padding: 0px;
+      }
+    </style
+  </head>
+  <body>
+    <table class="irc">
 <?php set_time_limit(1); echo "<tr><td bgcolor=\"" . str_replace(":", "\"></td></tr>\n<tr><td bgcolor=\"", str_replace(" ", "\"></td><td bgcolor=\"", str_replace("\n", "", preg_replace("/[^A-Za-z0-9: ]/", "", file_get_contents("config.txt"))))) . "\"></td></tr>\n"; ?>
+    </table>
+  </body>
+</html>
