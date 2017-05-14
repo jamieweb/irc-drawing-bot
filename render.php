c675e3d459b57511325738d694d5f189492c3697
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-05-14 15:26:25 +0100

    Create render.php

---
+++ render.php
@@ -0,0 +1,1 @@
+<?php set_time_limit(1); echo "<tr><td bgcolor=\"" . str_replace(":", "\"></td></tr>\n<tr><td bgcolor=\"", str_replace(" ", "\"></td><td bgcolor=\"", str_replace("\n", "", preg_replace("/[^A-Za-z0-9: ]/", "", file_get_contents("config.txt"))))) . "\"></td></tr>\n"; ?>
