67cee95266277352b2a9681fae89872ccab8a8f5
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-10-22 00:46:17 +0100

    Improved input sanitization

--- render.php
+++ render.php
@@ -10,7 +10,7 @@
   </head>
   <body>
     <table class="irc">
-<?php set_time_limit(1); echo "<tr><td bgcolor=\"" . str_replace(":", "\"></td></tr>\n<tr><td bgcolor=\"", str_replace(" ", "\"></td><td bgcolor=\"", str_replace("\n", "", preg_replace("/[^a-eghiklnoprtuwy: ]/", "", file_get_contents("grid.txt"))))) . "\"></td></tr>\n"; ?>
+<?php set_time_limit(1); echo "<tr><td bgcolor=\"" . str_replace(":", "\"></td></tr>\n<tr><td bgcolor=\"", str_replace(" ", "\"></td><td bgcolor=\"", str_replace("\n", "", preg_replace("/[^a-eghiklnoprtuwy: ]/", "", filter_var(strtolower(rtrim(substr(file_get_contents("grid.txt"), 0, 1536))), FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH))))) . "\"></td></tr>\n"; ?>
     </table>
   </body>
 </html>
